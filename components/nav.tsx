52788ddc05be1c3d228c633ba472396c139c4438
jonathanharrell <harr041@gmail.com>
2024-07-27 18:09:38 -0400

    WIP

--- components/nav.tsx
+++ components/nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const Nav = () => {
   return (
     <div className="md:w-[95vw] lg:w-[90vw] 2xl:w-[80vw] max-w-[1300px] mx-auto pt-4 px-8 sm:px-12 text-lg">
-      <p className="text-center small-caps">Human {`{ `}in the{` }`} Loop</p>
+      <p className="text-center small-caps">Human in the Loop</p>
       <nav className="py-1 pb-4 border-b border-neutral-200">
         <ul className="flex justify-center gap-3">
           <li><Link href="/">Home</Link></li>/
