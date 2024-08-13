8f398027e6a8a794cdbb5f2f2046c97fb2914894
jonathanharrell <harr041@gmail.com>
2024-08-12 20:50:40 -0400

    WIP

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -138,5 +138,5 @@ export const getPostMonths = async() => {
 export const getAllPhotoSlugs = async() => {
   const directoryPath = path.resolve(".", "public/assets/photos");
   const files = fs.readdirSync(directoryPath);
-  return files.reverse();
+  return files.filter(file => !file.endsWith(".mov") && !file.endsWith(".mp4")).reverse();
 }
