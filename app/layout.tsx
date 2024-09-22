c8bdbfe986d63f420716b2b69fa61b934f8adf12
jonathanharrell <harr041@gmail.com>
2024-09-22 12:09:59 -0400

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Jonathan Harrell",
-  description: "Jonathan Harrell's blog",
+  title: "Human in the Loop",
+  description: "Jonathan Harrell's creative scrapbook",
 };
 
 export default function RootLayout({
