9bb15c2ba12ec0b62ca7d27459c68d0c6bb82c11
jonathanharrell <harr041@gmail.com>
2024-10-23 21:28:14 -0400

    WIP

---
+++ app/sitemap.ts
@@ -0,0 +1,56 @@
+import type { MetadataRoute } from "next";
+import { SITE_URL } from "@/constants";
+import { getPostSlugs } from "@/lib/get-post-slugs";
+import { getAllPhotoSlugs } from "@/lib/get-all-post-slugs";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const { slugs: postSlugs = [] } = await getPostSlugs({ perPage: Infinity });
+  const photoSlugs = (await getAllPhotoSlugs()) ?? [];
+
+  return [
+    {
+      url: SITE_URL,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `${SITE_URL}blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...postSlugs.map((slug) => ({
+      url: `${SITE_URL}blog/${slug}`,
+      lastModified: new Date(),
+      changeFrequency:
+        "yearly" as MetadataRoute.Sitemap[number]["changeFrequency"],
+      priority: 0.8,
+    })),
+    {
+      url: `${SITE_URL}photos`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    ...photoSlugs.map((slug) => ({
+      url: `${SITE_URL}photo/${slug}`,
+      lastModified: new Date(),
+      changeFrequency:
+        "never" as MetadataRoute.Sitemap[number]["changeFrequency"],
+      priority: 0.5,
+    })),
+    {
+      url: `${SITE_URL}archive`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+    {
+      url: `${SITE_URL}about`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+  ];
+}
