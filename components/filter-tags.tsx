da2414ad3ddba84f291207727d9107c477e8988f
jonathanharrell <harr041@gmail.com>
2025-05-20 19:00:43 -0400

    Refactor filter components and improve accessibility.
    Extracted `FilterTags` into its own component for better modularity and maintainability. Enhanced accessibility attributes and responsiveness for filters, ensuring better user experience on both desktop and mobile.

---
+++ components/filter-tags.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { ChangeEventHandler } from "react";
+import { useRouter } from "next/navigation";
+import { Link } from "next-view-transitions";
+import { capitalize } from "lodash";
+import classNames from "classnames";
+
+interface FilterTagsProps {
+  tags: string[];
+  selectedTag?: string;
+}
+
+export const FilterTags = ({ tags, selectedTag }: FilterTagsProps) => {
+  const router = useRouter();
+
+  // used on mobile only
+  const filterByTag: ChangeEventHandler<HTMLSelectElement> = (event) => {
+    router.push(`/blog/filtered?tag=${event.target.value}`);
+  };
+
+  if (!tags.length) {
+    return null;
+  }
+
+  return (
+    <div className="col-span-12 lg:col-span-8 xl:col-span-7 flex flex-col gap-1">
+      <h3 id="tags-label" className="sr-only" aria-hidden="true">
+        Tags
+      </h3>
+      <div className="sm:hidden">
+        <p id="months-label" className="sr-only" aria-hidden="true">
+          Filter posts by month
+        </p>
+        <div>
+          <select
+            value={selectedTag}
+            onChange={filterByTag}
+            className="select appearance-none w-full py-2.5 pl-3 pr-8 rounded-full border border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 bg-transparent leading-none transition-colors duration-200 ease-in-out cursor-pointer"
+            aria-labelledby="months-label"
+          >
+            <option value="">All Tags</option>
+            {tags.map((tag) => (
+              <option key={tag} value={tag}>
+                {tag}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <ul
+        className="hidden sm:flex flex-wrap gap-2"
+        aria-labelledby="tags-label"
+      >
+        <li>
+          <Link
+            href={`/blog/filtered?tag=`}
+            className={classNames(
+              "block py-1.5 px-3 rounded-full border border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 leading-none text-neutral-500 transition-colors duration-200 ease-in-out",
+              !selectedTag
+                ? "border-neutral-400 text-neutral-800 dark:text-neutral-100"
+                : undefined,
+            )}
+            aria-label="Show all posts"
+          >
+            All
+          </Link>
+        </li>
+        {tags
+          .filter((tag) => tag !== "jdch")
+          .map((tag) => (
+            <li key={tag}>
+              <Link
+                href={`/blog/filtered?tag=${tag}`}
+                className={classNames(
+                  "block py-1.5 px-3 rounded-full border border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 leading-none text-neutral-500 transition-colors duration-200 ease-in-out",
+                  selectedTag?.includes(tag)
+                    ? "border-neutral-400 text-neutral-800 dark:text-neutral-100"
+                    : undefined,
+                )}
+                aria-label={`Filter posts by ${tag} tag`}
+              >
+                {capitalize(tag)}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
--- components/filters.tsx
+++ components/filters.tsx
@@ -1,6 +1,4 @@
-import { Link } from "next-view-transitions";
-import { capitalize } from "lodash";
-import classNames from "classnames";
+import { FilterTags } from "@/components/filter-tags";
 import { FilterMonths } from "@/components/filter-months";
 
 interface FiltersProps {
@@ -17,49 +15,11 @@ export const Filters = ({
   selectedMonth,
 }: FiltersProps) => {
   return (
-    <section id="filters" className="hidden sm:block mt-8">
+    <section id="filters" className="sm:mt-8">
       <h2 className="sr-only">Filters</h2>
-      <div className="grid grid-cols-12 gap-8 lg:gap-0 py-8 md:text-lg border-y border-neutral-200 dark:border-neutral-700">
+      <div className="grid grid-cols-12 gap-4 sm:gap-8 lg:gap-0 py-8 md:text-lg border-y border-neutral-200 dark:border-neutral-700">
         {Boolean(tags.length) && (
-          <div className="col-span-12 lg:col-span-8 xl:col-span-7 flex flex-col gap-1">
-            <h3 id="tags-label" className="sr-only" aria-hidden="true">
-              Tags
-            </h3>
-            <ul className="flex flex-wrap gap-2" aria-labelledby="tags-label">
-              <li>
-                <Link
-                  href={`/blog/filtered?tag=`}
-                  className={classNames(
-                    "block py-1.5 px-3 rounded-full border border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 leading-none text-neutral-500 transition-colors duration-200 ease-in-out",
-                    !selectedTag
-                      ? "border-neutral-400 text-neutral-800 dark:text-neutral-100"
-                      : undefined,
-                  )}
-                  aria-label="Show all posts"
-                >
-                  All
-                </Link>
-              </li>
-              {tags
-                .filter((tag) => tag !== "jdch")
-                .map((tag) => (
-                  <li key={tag}>
-                    <Link
-                      href={`/blog/filtered?tag=${tag}`}
-                      className={classNames(
-                        "block py-1.5 px-3 rounded-full border border-neutral-200 dark:border-neutral-700 hover:border-neutral-400 leading-none text-neutral-500 transition-colors duration-200 ease-in-out",
-                        selectedTag?.includes(tag)
-                          ? "border-neutral-400 text-neutral-800 dark:text-neutral-100"
-                          : undefined,
-                      )}
-                      aria-label={`Filter posts by ${tag} tag`}
-                    >
-                      {capitalize(tag)}
-                    </Link>
-                  </li>
-                ))}
-            </ul>
-          </div>
+          <FilterTags tags={tags} selectedTag={selectedTag} />
         )}
         <div className="col-span-12 lg:col-start-10 lg:pl-16 lg:border-l lg:border-neutral-200 dark:lg:border-neutral-700">
           <FilterMonths months={months} selectedMonth={selectedMonth} />
