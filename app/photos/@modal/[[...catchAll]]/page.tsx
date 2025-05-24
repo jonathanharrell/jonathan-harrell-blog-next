e5bf679df1bc38c9c25c2229d3440d4922ec4bfa
jonathanharrell <harr041@gmail.com>
2025-05-24 13:14:10 -0400

    Photo gallery WIP

--- app/photos/@modal/[[...catchAll]]/page.tsx
+++ app/photos/@modal/[[...catchAll]]/page.tsx
@@ -1,19 +1,5 @@
-import { getAllPhotoSlugs } from "@/lib/get-all-post-slugs";
-
 const PhotosPage = async () => {
   return null;
 };
 
 export default PhotosPage;
-
-export const generateMetadata = async () => {
-  const slugs = await getAllPhotoSlugs();
-
-  return {
-    title: "Photos | Human in the Loop",
-    description: "Photos from Jonathan Harrell’s commonplace book",
-    openGraph: {
-      images: [`/assets/photos/${slugs[0].slug}`],
-    },
-  };
-};
