ea59fa51f9942f97a1ece06aff59a064ca1e2a6a
jonathanharrell <harr041@gmail.com>
2025-05-24 13:09:56 -0400

    Photo gallery WIP

--- app/photo/[slug]/page.tsx
+++ app/photo/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { Photo } from "@/components/photo";
 import { getAllPhotoSlugs } from "@/lib/get-all-post-slugs";
-import { getPhotoMetadata } from "@/lib/get-photo-metadata";
+// import { getPhotoMetadata } from "@/lib/get-photo-metadata";
 
 interface PhotoPageProps {
   params: {
@@ -9,7 +9,7 @@ interface PhotoPageProps {
 }
 
 const PhotoPage = async ({ params }: PhotoPageProps) => {
-  const metadata = await getPhotoMetadata(params.slug);
+  // const metadata = await getPhotoMetadata(params.slug);
 
   return (
     <div className="wrapper flex items-center justify-center py-12">
@@ -17,7 +17,7 @@ const PhotoPage = async ({ params }: PhotoPageProps) => {
         slug={params.slug}
         width={1600}
         height={1600}
-        metadata={metadata}
+        // metadata={metadata}
         className="w-full h-full max-w-[1200px] max-h-[1200px]"
       />
     </div>
--- app/photos/@modal/(..)photo/[slug]/page.tsx
+++ app/photos/@modal/(..)photo/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { PhotoModal } from "@/components/photo-modal";
-import { getPhotoMetadata } from "@/lib/get-photo-metadata";
-import { getAllPhotoSlugs } from "@/lib/get-all-post-slugs";
+// import { getPhotoMetadata } from "@/lib/get-photo-metadata";
+// import { getAllPhotoSlugs } from "@/lib/get-all-post-slugs";
 
 interface PhotoPageProps {
   params: {
@@ -9,22 +9,22 @@ interface PhotoPageProps {
 }
 
 const PhotoPage = async ({ params }: PhotoPageProps) => {
-  const metadata = await getPhotoMetadata(params.slug);
-  const slugs = await getAllPhotoSlugs();
-
-  const matchingIndex = slugs.findIndex((item) => item.slug === params.slug);
-
-  const previousSlug = slugs[matchingIndex - 1];
-  const nextSlug = slugs[matchingIndex + 1];
+  // const metadata = await getPhotoMetadata(params.slug);
+  // const slugs = await getAllPhotoSlugs();
+  //
+  // const matchingIndex = slugs.findIndex((item) => item.slug === params.slug);
+  //
+  // const previousSlug = slugs[matchingIndex - 1];
+  // const nextSlug = slugs[matchingIndex + 1];
 
   return (
     <PhotoModal
       slug={params.slug}
       width={1600}
       height={1600}
-      metadata={metadata}
-      previousSlug={previousSlug?.slug}
-      nextSlug={nextSlug?.slug}
+      // metadata={metadata}
+      // previousSlug={previousSlug?.slug}
+      // nextSlug={nextSlug?.slug}
     />
   );
 };
