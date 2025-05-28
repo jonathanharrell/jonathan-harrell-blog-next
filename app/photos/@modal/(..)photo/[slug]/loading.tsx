6d4d2c0b34a6d37d6736a879d32c2476aaf17ace
jonathanharrell <harr041@gmail.com>
2025-05-27 21:32:43 -0400

    WIP

--- app/photos/@modal/(..)photo/[slug]/loading.tsx
+++ app/photos/@modal/(..)photo/[slug]/loading.tsx
@@ -1,9 +1,5 @@
-import { Spinner } from "@/components/spinner";
-
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center fixed inset-0 w-screen h-screen bg-neutral-900 bg-opacity-95">
-      <Spinner />
-    </div>
+    <div className="flex items-center justify-center fixed inset-0 w-screen h-screen bg-neutral-900 bg-opacity-95" />
   );
 }
